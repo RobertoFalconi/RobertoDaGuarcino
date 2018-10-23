761b85a641057a1ea0c5ae039560c2bd97e4431b
RobertoFalconi <falkons@outlook.com>
2018-10-23 11:20:23 +0200

    Donut chart and code cleaning

--- RobertoDaGuarcino/wwwroot/js/pages/index.js
+++ RobertoDaGuarcino/wwwroot/js/pages/index.js
@@ -72,25 +72,21 @@ function initDonutChart() {
     Morris.Donut({
         element: 'donut_chart',
         data: [{
-            label: 'Chrome',
-            value: 37
+            label: 'PC',
+            value: 252
         }, {
-            label: 'Firefox',
-            value: 30
+            label: 'Xbox',
+            value: 161
         }, {
-            label: 'Safari',
-            value: 18
+            label: 'PlayStation',
+            value: 58
         }, {
-            label: 'Opera',
-            value: 12
-        },
-        {
-            label: 'Other',
-            value: 3
+            label: 'Nintendo',
+            value: 150
         }],
-        colors: ['rgb(233, 30, 99)', 'rgb(0, 188, 212)', 'rgb(255, 152, 0)', 'rgb(0, 150, 136)', 'rgb(96, 125, 139)'],
+        colors: ['grey', 'green', 'blue', 'red'],
         formatter: function (y) {
-            return y + '%'
+            return y; // + '%'
         }
     });
 }
