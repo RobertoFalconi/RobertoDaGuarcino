9f605f64a662ecea48a744d586cb84935f51e747
RobertoFalconi <falkons@outlook.com>
2018-10-24 11:50:25 +0200

    Cleaning

--- RobertoDaGuarcino/Controllers/HomeController.cs
+++ RobertoDaGuarcino/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using RobertoDaGuarcino.Models;
 
 namespace RobertoDaGuarcino.Controllers
 {
@@ -36,9 +35,10 @@ namespace RobertoDaGuarcino.Controllers
 
             return View();
         }
+
         public IActionResult Error()
         {
-            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
             return View();
         }
     }
