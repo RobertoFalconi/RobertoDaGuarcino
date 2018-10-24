003126f9b929ae8c0a5e55d00c2298848b5461ae
RobertoFalconi <falkons@outlook.com>
2018-10-24 11:37:04 +0200

    Cleaning

--- RobertoDaGuarcino/Controllers/HomeController.cs
+++ RobertoDaGuarcino/Controllers/HomeController.cs
@@ -38,7 +38,8 @@ namespace RobertoDaGuarcino.Controllers
         }
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
