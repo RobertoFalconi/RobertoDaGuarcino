7a3cf15410cc3911f7bf651fd6b314316f87c8d7
RobertoFalconi <falkons@outlook.com>
2020-01-31 15:39:44 +0100

    CSP base-uri

--- RobertoDaGuarcino/Startup.cs
+++ RobertoDaGuarcino/Startup.cs
@@ -55,7 +55,7 @@ namespace RobertoDaGuarcino
                     context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                     context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
                     context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                    context.Response.Headers.Add("Content-Security-Policy", "script-src 'self'; object-src 'self'");
+                    context.Response.Headers.Add("Content-Security-Policy", "base-uri: 'self'; script-src 'self'; object-src 'self'");
                     context.Response.Headers.Add("Referrer-Policy", "same-origin");
                     context.Response.Headers.Add("Feature-Policy", "geolocation 'self'; midi 'self'; notifications 'self'; push 'self'; sync-xhr 'self'; microphone 'self'; camera 'self'; magnetometer 'self'; gyroscope 'self'; speaker 'self'; vibrate 'self'; fullscreen 'self'; payment 'self';"); 
                     return Task.FromResult(0);
