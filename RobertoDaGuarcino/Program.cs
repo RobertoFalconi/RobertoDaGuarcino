793e5be9785ae02d2e3e9d8dbe1a9575b0eae0f6
RobertoFalconi <falkons@outlook.com>
2022-04-22 15:08:04 +0200

    Refactoring

--- RobertoDaGuarcino/Program.cs
+++ RobertoDaGuarcino/Program.cs
@@ -20,9 +20,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
