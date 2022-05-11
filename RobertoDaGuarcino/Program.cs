103dbaa0d2158d356cf04b2eade7b2968a224f82
RobertoFalconi <falkons@outlook.com>
2022-05-11 14:56:54 +0200

    Refactoring

--- RobertoDaGuarcino/Program.cs
+++ RobertoDaGuarcino/Program.cs
@@ -4,9 +4,23 @@ using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();//.AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
+//app.UseExceptionHandler("/Home/Error");
+
+//app.UseHsts();
+//app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapRazorPages();
 
 app.Run();
